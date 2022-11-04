29ba9986e04a167903799c35bd7846a9bebe465a
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-11-03 22:14:40 -0500

    fix: Updates the title

--- src/Screen/Future/Future.js
+++ src/Screen/Future/Future.js
@@ -7,7 +7,7 @@ function Future() {
     return (
         <>
             <div className="body-header border-bottom d-flex py-3 mb-3">
-                <PageTitle pagetitle='Future Trade' />
+                <PageTitle pagetitle='Test Market' />
             </div>
             <div className='container-xxl'>
                 <div className='row g-3 mb-3 row-deck'>
