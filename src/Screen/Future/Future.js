82346360c35b5a61aea6c286d89e03fab7225577
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-11-03 22:13:41 -0500

    fix: Removes side buttons

--- src/Screen/Future/Future.js
+++ src/Screen/Future/Future.js
@@ -9,7 +9,7 @@ function Future() {
     return (
         <>
             <div className="body-header border-bottom d-flex py-3 mb-3">
-                <PageTitle pagetitle='Future Trade' downloadbutton='Download' reportbutton='Generate Report' />
+                <PageTitle pagetitle='Future Trade' />
             </div>
             <div className='container-xxl'>
                 <div className='row g-3 mb-3 row-deck'>
